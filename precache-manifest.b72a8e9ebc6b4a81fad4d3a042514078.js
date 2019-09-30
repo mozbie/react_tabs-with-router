b0741e0846153c1323f4d9a5a5cb00c8cd19cb3c
Alina Kizilova <alinakzilova@gmail.com>
2019-09-30 23:53:50 +0300

    Updates

---
+++ precache-manifest.b72a8e9ebc6b4a81fad4d3a042514078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde44c1c7fc34983fe4cfb357fd91269",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "e59aa68ee77919b0b0bf",
+    "url": "/react_tabs-with-router/static/css/main.aec80d55.chunk.css"
+  },
+  {
+    "revision": "ed190164e008513b3cb6",
+    "url": "/react_tabs-with-router/static/js/2.53677fd4.chunk.js"
+  },
+  {
+    "revision": "e59aa68ee77919b0b0bf",
+    "url": "/react_tabs-with-router/static/js/main.c6364625.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
